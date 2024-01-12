a696d327856943f52981f7af4fc4d517a3552ae2
ShioriAya <shioriaya@foxmail.com>
2024-01-13 04:17:22 +0800

    1.12

--- blue_arxiv/mainpage/templatetags/filters.py
+++ blue_arxiv/mainpage/templatetags/filters.py
@@ -12,3 +12,13 @@ def highlight(value, keyword):
         return f'<span style="color: red; font-weight: bolder;">{ match.group() }</span>'
     
     return regex.sub(callback, value)
+
+@register.filter(name='nameparser')
+def nameparser(value):
+    namelist = []
+    
+    for name in value.split(','):
+        regex = re.compile(r'\s*and\s*', re.IGNORECASE)
+        namelist.append(", ".join([f'<span style="text-decoration-line: underline; cursor: pointer;" onclick="uploadform(this.innerText)">{ name_separate }</span>' for name_separate in regex.split(name) if name_separate is not ""]))
+    
+    return ", ".join(namelist)
